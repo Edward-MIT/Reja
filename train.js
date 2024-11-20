f0cb736b7236c741aa4ed4ad63e81215ec9436f1
Edward_MIT <usmanoff19980404@gmail.com>
2024-11-20 20:05:30 +0900

    feat : lesson callback and async function

--- train.js
+++ train.js
@@ -1,1 +1,15 @@
-console.log("train task ishga tushdi!");
+//  A - task
+/* shunday ikki parametrli function tuzing , birinchi parametrdagi letterni
+ikkinchi parametrdagi sozdan qatnashgan sonini return qilishi kerak bo'ladi
+Masalan Countletter(" e " , "engineer") 3ni return qilsin  */
+
+// Masalani yechimi
+
+function countLetter (word, number(){
+  if ()
+}); {
+  let
+
+  countLetter
+
+}
