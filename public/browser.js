4750469fc8ddcbb230e20b4f079dfe5f68fdfa42
Edward_MIT <usmanoff19980404@gmail.com>
2024-11-29 01:18:01 +0900

    feat: CRUD is finished

--- public/browser.js
+++ public/browser.js
@@ -1,3 +1,4 @@
+// const { response } =require("./app");
 
 console.log("Frontend JavaScript ishga tushdi");
 
@@ -47,6 +48,21 @@ document.addEventListener("click", function(e){
 
 //edit operation
  if(e.target.classList.contains("edit-me")) {
-   alert("siz edit tugmasini bosdingiz");
-  }
+ let userInput = prompt("o'zgartirish kiriting", e.target.parentElement.parentElement.querySelector(".item-text").innerHTML);
+ if(userInput) {
+  axios.post("/edit-item",{id: e.target.getAttribute("data-id"), new_input: userInput}).then(response => {
+  console.log(response);
+  e.target.parentElement.parentElement.querySelector(".item-text").innerHTML = userInput;
+  }).catch(err => {
+  console.log("iltimos qaytadan harakat qiling");
+  });
+ }
+}
+});
+
+document.getElementById("clean-all").addEventListener("click", function(){
+  axios.post("/delete-all", {delete_all: true}).then((response) =>{
+    alert(response.data.state);
+    document.location.reload();
+  });
 });
--- train.js
+++ train.js
@@ -1,3 +1,77 @@
+// D-TASK:
+
+// Shunday function tuzing, u 2ta string parametr ega bolsin, hamda agar har ikkala string bir hil harflardan iborat bolsa true aks holda false qaytarsin
+// MASALAN checkContent("mitgroup", "gmtiprou") return qiladi true;
+
+
+function checkContent(string1, string2) {
+  const str1 = string1.split("").sort().join('');
+  const str2 = string2.split('').sort().join('');
+
+  return str1 === str2;
+
+
+ }
+
+
+console.log(checkContent('mengg','nem'));
+
+
+
+
+
+
+// C- Task
+// MITASK-C
+
+// Shunday class tuzing tuzing nomi Shop, va uni constructoriga 3 hil mahsulot pass bolsin, hamda classning 3ta methodi bolsin, biri qoldiq, biri sotish va biri qabul. Har bir method ishga tushgan vaqt ham log qilinsin.
+// MASALAN: const shop = new Shop(4, 5, 2); shop.qoldiq() return hozir 20:40da 4ta non, 5ta lagmon va 2ta cola mavjud! shop.sotish('non', 3) & shop.qabul('cola', 4) & shop.qoldiq() return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud!
+
+
+// Masalani yechimi
+
+// const moment = require('moment');
+
+// const now = new Date;
+// const hozir = now.toLocaleTimeString();
+
+// class Shop {
+
+//     constructor(non, lagmon, cola){
+//       this.non = non;
+//       this.lagmon = lagmon;
+//       this.cola = cola;
+//     }
+
+//    qoldiq(){
+//      console.log(`Hozir ${hozir}da  ${this.non} ta non, ${this.lagmon} ta lagmon va ${this.cola} ta cola mavjud!`);
+//    }
+//     sotish(non, raqam){
+//      this.non = this.non - raqam;
+//      console.log(`Hozir ${hozir} da ${this.non} ta non ${this.lagmon} ta lagmon va ${this.cola} ta cola mavjud! `);
+//    }
+//    qabul(cola, raqam){
+
+//     console.log(`Hozir  ${hozir} da ${this.non} ta non ${this.lagmon} ta lagmon va ${this.cola} ta cola mavjud!` );
+//    }
+//     qoldiq(){
+//      console.log(`Hozir ${hozir}da  ${this.non} ta non, ${this.lagmon} ta lagmon va ${this.cola} ta cola mavjud!`);
+//    }
+
+
+//   }
+//  const shop = new Shop(5, 5, 2);
+//   shop.qoldiq();
+//   shop.sotish('non',5);
+//   shop.qabul('cola',4);
+//   shop.qoldiq();
+
+
+
+
+
+
+
 
 // B-TASK:
 
@@ -7,17 +81,17 @@
 
 // masalani yechimi
 
-function countLetter(inputString) {
-     let count = 0;
-     inputString.split("").forEach(char => {
-         if (char === char) {
-             count++;
-         }
-     });
-     return count;
-   }
+// function countLetter(inputString) {
+//      let count = 0;
+//      inputString.split("").forEach(char => {
+//          if (char === char) {
+//              count++;
+//          }
+//      });
+//      return count;
+//    }
 
-   console.log(countLetter("hello666666"));
+//    console.log(countLetter("hello666666"));
 
 
 
